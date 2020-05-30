63beb7a8e0d69fa423743f1422f297e2dbf67a1b
henry lay <henry88lay@gmail.com>
2020-05-30 14:23:18 +1000

    added the like model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_040753) do
+ActiveRecord::Schema.define(version: 2020_05_30_042234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,15 @@ ActiveRecord::Schema.define(version: 2020_05_30_040753) do
     t.index ["user_id"], name: "index_galleries_on_user_id"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "artwork_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["artwork_id"], name: "index_likes_on_artwork_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -86,4 +95,6 @@ ActiveRecord::Schema.define(version: 2020_05_30_040753) do
   add_foreign_key "favourites", "artworks"
   add_foreign_key "favourites", "users"
   add_foreign_key "galleries", "users"
+  add_foreign_key "likes", "artworks"
+  add_foreign_key "likes", "users"
 end
