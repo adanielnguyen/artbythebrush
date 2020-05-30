d640f2c0c85650bc521fc81cca8027370028967c
henry lay <henry88lay@gmail.com>
2020-05-30 13:43:51 +1000

    added the artwork model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_022246) do
+ActiveRecord::Schema.define(version: 2020_05_30_033656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artworks", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "genre"
+    t.bigint "user_id"
+    t.string "medium"
+    t.date "date_created"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_artworks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +43,5 @@ ActiveRecord::Schema.define(version: 2020_05_30_022246) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "artworks", "users"
 end
