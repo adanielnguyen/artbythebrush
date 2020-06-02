116f0099df9b4364370eef9d606fe27c92235c2e
Daniel Nguyen <daniel-nguyen@live.com.au>
2020-06-02 20:05:45 +1000

    Completed Seed File

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+
 FollowedRelationship.destroy_all
 User.destroy_all
 Tag.destroy_all
+Artwork.destroy_all
 
-user1 = User.create!(email: "user1@hotmail.com", password:"password")
+user1 = User.create!(email: "henry", password:"password")
 user2 = User.create!(email: "user2@hotmail.com", password:"password")
 user3 = User.create!(email: "user3@hotmail.com", password:"password")
 user4 = User.create!(email: "user4@hotmail.com", password:"password")
@@ -23,3 +26,23 @@ FollowedRelationship.create!(record: oil, user: user1)
 FollowedRelationship.create!(record: user2, user: user1)
 FollowedRelationship.create!(record: user3, user: user1)
 FollowedRelationship.create!(record: black, user: user1)
+
+artwork1 = Artwork.create!(name: "Water Lilies 1906 - Claude Monet", description: "One instant, one aspect of nature contains it all, said Claude Monet, referring to his late masterpieces, the water landscapes that he produced at his home in Giverny between 1897 and his death in 1926. These works replaced the varied contemporary subjects he had painted from the 1870s through the 1890s with a single, timeless motif—water lilies. The focal point of these paintings was the artist’s beloved flower garden, which featured a water garden and a smaller pond spanned by a Japanese footbridge.", genre: "Impressionism Painting", user_id: 1, medium: "Oil Painting"  )
+file = URI.open('http://art-monet.com/image/1900/1906%20Water%20Lilies7.jpg')
+artwork1.image.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+artwork1.save!
+
+artwork2 = Artwork.create!(name: "Water Lilies 1906 - Claude Monet", description: "One instant, one aspect of nature contains it all, said Claude Monet, referring to his late masterpieces, the water landscapes that he produced at his home in Giverny between 1897 and his death in 1926. These works replaced the varied contemporary subjects he had painted from the 1870s through the 1890s with a single, timeless motif—water lilies. The focal point of these paintings was the artist’s beloved flower garden, which featured a water garden and a smaller pond spanned by a Japanese footbridge.", genre: "Impressionism Painting", user_id: 1, medium: "Oil Painting" )
+file = URI.open('http://art-monet.com/image/1900/1906%20Water%20Lilies.jpg')
+artwork2.image.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+artwork2.save!
+
+artwork3 = Artwork.create!(name: "Claude Monet - Water Lilies 1907", description: "One instant, one aspect of nature contains it all, said Claude Monet, referring to his late masterpieces, the water landscapes that he produced at his home in Giverny between 1897 and his death in 1926. These works replaced the varied contemporary subjects he had painted from the 1870s through the 1890s with a single, timeless motif—water lilies. The focal point of these paintings was the artist’s beloved flower garden, which featured a water garden and a smaller pond spanned by a Japanese footbridge.", genre: "Impressionism Painting", user_id: 1, medium: "Oil Painting" )
+file = URI.open('http://art-monet.com/image/1900/1907%20Water%20Lilies%20(2).jpg')
+artwork3.image.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+artwork3.save!
+
+artwork4 = Artwork.create!(name: "Claude Monet - Floating Ice on the Seine 1880", description: "One instant, one aspect of nature contains it all, said Claude Monet, referring to his late masterpieces, the water landscapes that he produced at his home in Giverny between 1897 and his death in 1926. These works replaced the varied contemporary subjects he had painted from the 1870s through the 1890s with a single, timeless motif—water lilies. The focal point of these paintings was the artist’s beloved flower garden, which featured a water garden and a smaller pond spanned by a Japanese footbridge.", genre: "Impressionism Painting", user_id: 1, medium: "Oil Painting" )
+file = URI.open('http://art-monet.com/image/1880/1880%20Floating%20Ice%20on%20the%20Seine.jpg')
+artwork4.image.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+artwork4.save!
