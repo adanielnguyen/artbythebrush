8696d967dff7e438dc6abd9455ac0b1e993aeee6
henry lay <henry88lay@gmail.com>
2020-06-06 11:40:28 +1000

    fixed up the relationships for the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   has_many :follows, as: :record
-  has_many :follows
+  has_many :fans, through: :follows
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  def followed_items
+    Follow.where(fan: self)
+  end
 
 end
