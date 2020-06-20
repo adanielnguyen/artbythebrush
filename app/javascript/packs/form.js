e703f6600b9a1fc20e38b7300fb5219a9ab204c4
henry lay <henry88lay@gmail.com>
2020-06-20 14:53:16 +1000

    appled all the changes from marty

--- app/javascript/packs/form.js
+++ app/javascript/packs/form.js
@@ -1,5 +1,4 @@
 var loadFile = function(event) {
-  console.log("im in")
   var output = document.getElementById('output');
   output.style.backgroundImage = 'url(' + URL.createObjectURL(event.target.files[0]) + ')';
   output.onload = function() {
