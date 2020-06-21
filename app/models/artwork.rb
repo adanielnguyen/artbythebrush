479ce35183571c6f081b1f760bf6099616b676ad
henry lay <henry88lay@gmail.com>
2020-06-21 21:41:00 +1000

    added validations for artworks form

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -2,4 +2,6 @@ class Artwork < ApplicationRecord
   has_many_attached :images
   belongs_to :user
   acts_as_taggable_on :tags
+
+  validates :name, :description, :genre, :medium, :date_created, :images, presence: true
 end
