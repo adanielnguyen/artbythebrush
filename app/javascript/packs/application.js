a20b427dfb4429d579a14af8019dcfc331052da1
henry lay <henry88lay@gmail.com>
2020-06-26 10:39:25 +1000

    removed the .start() on applicatino js on form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("packs/form").start()
+require("packs/form")
 require("packs/btn-bespoke")
 require("../custom.js")
 
